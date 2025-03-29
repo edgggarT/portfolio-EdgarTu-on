af085cde6e3a4905da2e673e5a256a09424a4fca
Edgar Tuñon <edgartu@outlook.com>
2025-03-28 21:19:58 -0300

    Mas cambios en flask y html base

--- app.py
+++ app.py
@@ -4,26 +4,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template('index.html')
+    return render_template('index.html', nav_title="inicio")
 
 @app.route('/contactame')
 def contact():
-    return render_template('contactame.html')
+    return render_template('contactame.html', nav_title="contacto")
 
 
 @app.route('/proyetos')
 def proyectos():
-    return render_template('proyectos.html')
+    return render_template('proyectos.html', nav_title="proyectos")
 
 
 @app.route('/lenguajes')
 def lenguajes():
-    return render_template('lenguajes.html')
+    return render_template('lenguajes.html', nav_title="lenguajes")
 
 
 @app.route('/AcercaDe')
 def acerca_de():
-    return render_template('Acerca_de.html')
+    return render_template('Acerca_de.html', nav_title="acerca de")
 
 
 if __name__=='__main__':
